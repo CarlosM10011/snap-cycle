650ae2436c10577d167e4b48b23685abc4c5417c
Carlos <webmaster@carlos1001.com>
2018-07-23 11:20:57 -0500

    Add search handler
    Added search handler for search page, but there isn't a page in this
    commit. the python script has the template mapped to
    src/templates/search.html.

--- src/main.py
+++ src/main.py
@@ -7,6 +7,11 @@ from google.appengine.ext import ndb
 jinja_current_directory = os.path.dirname(__file__)
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(jinja_current_directory), extensions=['jinja2.ext.autoescape'], autoescape=True)
 
+class SearchHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        search_template = jinja_env.get_template("templates/search.html")
+        self.response.out.write(search_template.render())
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
@@ -17,4 +22,5 @@ class MainHandler(webapp2.RequestHandler):
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
+    (‘/search’, SearchHandler),
     ], debug=False)
