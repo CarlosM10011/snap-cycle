4ec63b9d74449a6a4dd7085f216c8c1143e0f7aa
Hana <demouser@MacBook-Pro.local>
2018-07-24 17:28:57 -0700

    idk

--- src/js/script.js
+++ src/js/script.js
@@ -14,3 +14,12 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+function myMap() {
+var mapOptions = {
+    center: new google.maps.LatLng(51.5, -0.12),
+    zoom: 10,
+    mapTypeId: google.maps.MapTypeId.HYBRID
+}
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
